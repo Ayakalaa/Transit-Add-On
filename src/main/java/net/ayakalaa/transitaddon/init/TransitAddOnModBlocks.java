4faff41333b025b292c9f18acc11531ce3bb8b36
Ayakalaa <win7ult09@gmail.com>
2023-12-24 20:00:05 -0600

    TAOModBlocks Fix

--- src/main/java/net/ayakalaa/transitaddon/init/TransitAddOnModBlocks.java
+++ src/main/java/net/ayakalaa/transitaddon/init/TransitAddOnModBlocks.java
@@ -28,5 +28,5 @@ public class TransitAddOnModBlocks {
 	public static final RegistryObject<Block> RED_PYLON = REGISTRY.register("red_pylon", () -> new RedPylon());
 	public static final RegistryObject<Block> ORANGE_PYLON = REGISTRY.register("orange_pylon", () -> new OrangePylon());
 	public static final RegistryObject<Block> YELLOW_PYLON = REGISTRY.register("yellow_pylon", () -> new YellowPylon());
-	public static final RegistryObject<Block> GREEN_PYLON = REGISTRY.register("green_pylon", () -> new GreenPylon())
+	public static final RegistryObject<Block> GREEN_PYLON = REGISTRY.register("green_pylon", () -> new GreenPylon());
 }
